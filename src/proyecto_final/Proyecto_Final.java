eaa172c373ed8efb2f360c95b4605ed07340a5b0
Gabriela <Gabriela@192.168.8.75>
2025-05-15 18:21:02 -0600

    Primer Proyecto

---
+++ src/proyecto_final/Proyecto_Final.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_final;
+
+import static java.time.Clock.system;
+import static java.time.InstantSource.system;
+
+/**
+ *
+ * @author Gabriela
+ */
+public class Proyecto_Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("H");
+    }
+    
+}

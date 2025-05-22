b7f71369d5a53358fff4d46f37c211586d336b35
Jimena18-2023 <jimemenendez2019@gmail.com>
2025-05-22 05:18:19 -0600

    actualizacion de controlador y dao de gestion de productos

--- src/GESTIONPRODUCTOSCONTROL/CONTROLADOR.java
+++ src/GESTIONPRODUCTOSCONTROL/CONTROLADOR.java
@@ -4,15 +4,29 @@
  */
 package GESTIONPRODUCTOSCONTROL;
 
+import gestionProductos.DAO.DAO;
+import gestionProductosModelo.Modelo;
+import java.util.List;
+
+
 /**
  *
  * @author jimem
  */
 public class CONTROLADOR {
-    private gestionProductos productoDAO;
-
-    public CONTROLADOR() {
-        
+    private final gestionProductos.DAO.DAO dao = new gestionProductos.DAO.DAO();
+    
+    public List<Modelo> obtenerProductos(){
+        return dao.obtenerTodos();
     }
     
+    public void insertarPedido (String nombre, int cantidad, double precio){
+        Modelo producto = new Modelo(0, nombre, cantidad, precio);
+        dao.insertar(producto);
+    }
+    
+    public void actualizarPedido (int id, String nombre, int cantidad, double precio){
+        Modelo producto = new Modelo(0, nombre, cantidad, precio);
+        dao.actualizar(producto);
+    }
 }
--- src/gestionProductos/DAO/DAO.java
+++ src/gestionProductos/DAO/DAO.java
@@ -16,7 +16,64 @@ import java.util.List;
 public class DAO {
     private final CreateConection connFactory = new CreateConection();
     
-    public List<Producto> obtenerTodos(){
-            List<Producto> lista = new ArrayList<>();
+    public List<Modelo> obtenerTodos(){
+            List<Modelo> lista = new ArrayList<>();
+            String sql = "SELECT FROM producto";
+            try(Connection conn = connFactory.getConnection();
+                    PreparedStatement ps = conn.prepareStatement(sql);
+                    ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+                    Modelo producto = new Modelo(
+                    rs.getInt("id"),
+                    rs.getString("nombre"),
+                    rs.getInt("cantidad"),
+                    rs.getDouble("precio")
+                    );
+                    lista.add(producto);
+                 }
+                ps.close();
+                conn.close();
+            } catch (SQLException e){
+                e.printStackTrace();
             }
+            return lista;
+    }
+    public boolean insertar(Modelo producto){
+        String sql = "INSERT INTO producto (nombre, cantidad, precio) VALUES (?,?,?)";
+        
+        try(Connection conn = connFactory.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setString(1, producto.getProduct_name());
+            ps.setInt(2, producto.getCantidad());
+            ps.setDouble(3, producto.getProduct_price());
+            
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+            return true;
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    
+    public boolean actualizar(Modelo producto){
+       String sql = "UPDATE producto SET nombre=?, cantidad=?, precio=? WHERE id=?";
+        
+        try(Connection conn = connFactory.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setString(1, producto.getProduct_name());
+            ps.setInt(2, producto.getCantidad());
+            ps.setDouble(3, producto.getProduct_price());
+            ps.setInt(4, producto.getId_product());
+            
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+            return true;
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return false; 
+    }
 }
